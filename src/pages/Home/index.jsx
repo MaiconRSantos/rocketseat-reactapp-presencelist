5549dca3c896c1bb8d0470105950f0a5981e7365
MaiconRSantos <maiconrsantos96@gmail.com>
2022-06-26 11:35:44 -0300

    Final commit - conclusão do curso de fundamentos do React

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
 import "./styles.css";
 import { Card } from "../../components/Card";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function Home() {
   const [studantName, setStudantName] = useState("");
   const [studants, setStudants] = useState([]);
-  
+  const [user, setUser] = useState({name: '', avatar: ''})
+
   function handleAddStudant(){
 
     const newStudant = {
@@ -21,9 +22,51 @@ export function Home() {
     setStudants(prevState => [...prevState, newStudant])
   }
 
+  useEffect(() => {
+    //corpo do useEffect
+
+    // Usando async:
+
+    async function fetchData(){
+      const response = await fetch('https://api.github.com/users/MaiconRSantos')
+      const data = await response.json();
+      
+
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      })
+
+   
+    
+    }
+
+    fetchData()
+
+    // fetch('https://api.github.com/users/MaiconRSantos')
+    // .then(response => response.json())
+    // .then(data => {
+    //   setUser({
+    //     name: data.name,
+    //     avatar: data.avatar_url
+    //   })
+    // })
+
+
+  }, [])
+
   return (
-    <form className="container">
+
+    <div className="container">
+
+    <header>
       <h1>Lista de Presença</h1>
+      <div>
+      <strong>{user.name}</strong>
+      <img src={user.avatar} alt="Foto de perfil" />
+      </div>
+    </header>
+
       <input
         type="text"
         placeholder="Digite o nome..."
@@ -36,7 +79,6 @@ export function Home() {
         studants.map(studant => <Card key={studant.time} name={studant.name} time={studant.time} />)
       }
 
-
-    </form>
+    </div>
   )
 }
